49b06b0326b15706c4aca8d5226b76e4b5d8fe66
DiegoBlazquezRosado <diegoblazquezr@gmail.com>
2024-06-03 23:54:48 +0200

    working but needs cleaning

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,16 @@ const sectionAllBooksContainer = document.querySelector('#all-books-container');
 const h3AllBooks = document.querySelector('#h3-all-books');
 const backButtonAllBooksContainer = document.querySelector('#back-button-all-books-container');
 
+const paginationContainer = document.querySelector('#pagination-container');
+let currentPage = 1;
+const amountPerPage = 5;
+let amountOfPages;
+let genre;
+
 const AllLists = 'https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=CmHFGOrHUTetIVGxsAImRJXCHxEhTajD';
 
-sectionAllListsContainer.addEventListener('click', ({ target }) => {
+document.addEventListener('click', ({ target }) => {
+
     if (target.matches('#all-lists-container button')) {
         genre = target.value;
         sectionAllListsContainer.innerHTML = '';
@@ -15,11 +22,11 @@ sectionAllListsContainer.addEventListener('click', ({ target }) => {
         h3AllBooks.classList.remove('hide');
         backButtonAllBooksContainer.classList.remove('hide');
         sectionAllBooksContainer.classList.remove('hide');
+        paginationContainer.innerHTML = '';
+        paginationContainer.classList.remove('hide');
         getAllBooks(genre);
     }
-});
 
-backButtonAllBooksContainer.addEventListener('click', ({ target }) => {
     if (target.matches('#back-button-all-books')) {
         h3AllBooks.innerHTML = '';
         h3AllBooks.classList.add('hide');
@@ -28,7 +35,24 @@ backButtonAllBooksContainer.addEventListener('click', ({ target }) => {
         backButtonAllBooksContainer.classList.add('hide');
         sectionAllBooksContainer.classList.add('hide');
         sectionAllListsContainer.classList.remove('hide');
+        paginationContainer.classList.add('hide');
         getAllLists();
+    }
+
+    if (target.matches('#pagination-back')) {
+        currentPage--;
+        backButtonAllBooksContainer.innerHTML = '';
+        sectionAllBooksContainer.innerHTML = '';
+        paginationContainer.innerHTML = '';
+        getAllBooks(genre);
+    }
+
+    if (target.matches('#pagination-forward')) {
+        currentPage++;
+        backButtonAllBooksContainer.innerHTML = '';
+        sectionAllBooksContainer.innerHTML = '';
+        paginationContainer.innerHTML = '';
+        getAllBooks(genre);
     }
 });
 
@@ -36,13 +60,9 @@ const getAllLists = async () => {
     try {
         loader.classList.remove('hide');
         const responseAllLists = await fetch(AllLists);
-        if (responseAllLists.ok) {
-            const dataAllLists = await responseAllLists.json();
-            const arrAllLists = dataAllLists.results;
-            return paintAllLists(arrAllLists);
-        } else {
-            throw responseAllLists;
-        }
+        const dataAllLists = await responseAllLists.json();
+        const arrAllLists = dataAllLists.results;
+        return paintAllLists(arrAllLists);
     } catch (error) {
         throw error;
     }
@@ -50,8 +70,8 @@ const getAllLists = async () => {
 
 const paintAllLists = (arrAllLists) => {
 
-    arrAllLists.forEach((element) => {
-        const divAllLists = document.createElement('div');
+    arrAllLists.forEach(element => {
+        const articleAllLists = document.createElement('article');
         const h3AllLists = document.createElement('h3');
         h3AllLists.innerText = element.list_name;
         const hrAllLists = document.createElement('hr');
@@ -65,8 +85,8 @@ const paintAllLists = (arrAllLists) => {
         buttonAllLists.innerText = 'READ MORE! >';
         buttonAllLists.value = element.list_name_encoded;
 
-        divAllLists.append(h3AllLists, hrAllLists, pAllListsOldest, pAllListsNewest, pAllListsUpdated, buttonAllLists);
-        fragment.append(divAllLists);
+        articleAllLists.append(h3AllLists, hrAllLists, pAllListsOldest, pAllListsNewest, pAllListsUpdated, buttonAllLists);
+        fragment.append(articleAllLists);
     });
     sectionAllListsContainer.append(fragment);
     loader.classList.add('hide');
@@ -76,28 +96,34 @@ const getAllBooks = async (genre) => {
     try {
         loader.classList.remove('hide');
         const responseAllBooks = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${genre}.json?api-key=CmHFGOrHUTetIVGxsAImRJXCHxEhTajD`);
-        if (responseAllBooks.ok) {
-            const dataAllBooks = await responseAllBooks.json();
-            const arrAllBooks = dataAllBooks.results.books;
-            return paintAllBooks(dataAllBooks, arrAllBooks);
-        } else {
-            throw responseAllBooks;
-        }
+        const dataAllBooks = await responseAllBooks.json();
+        const arrAllBooks = dataAllBooks.results.books;
+
+        const start = (currentPage - 1) * amountPerPage;
+        const end = start + amountPerPage;
+        const arrAllBooksSliced = arrAllBooks.slice(start, end);
+
+        amountOfPages = Math.ceil(arrAllBooks.length / amountPerPage);
+
+        return paintAllBooks(dataAllBooks, arrAllBooksSliced);
     } catch (error) {
         throw error;
     }
 }
 
-const paintAllBooks = (dataAllBooks, arrAllBooks) => {
-    
+const paintAllBooks = (dataAllBooks, arrAllBooksSliced) => {
+
     window.scrollTo(0, 0);
     h3AllBooks.innerText = dataAllBooks.results.display_name;
+
+    //Return back to All Lists Button
     const backButtonAllBooks = document.createElement('button');
     backButtonAllBooks.innerText = '< BACK TO INDEX';
     backButtonAllBooks.id = 'back-button-all-books';
 
-    arrAllBooks.forEach((element) => {
-        const divAllBooks = document.createElement('div');
+    //All Books Cards Elements
+    arrAllBooksSliced.forEach(element => {
+        const articleAllBooks = document.createElement('article');
         const h4AllBooks = document.createElement('h4');
         h4AllBooks.innerHTML = `#${element.rank} ${element.title}`;
         const imgAllBooks = document.createElement('img');
@@ -112,11 +138,37 @@ const paintAllBooks = (dataAllBooks, arrAllBooks) => {
         buttonAllBooks.innerText = 'BUY AT AMAZON >';
         aAllBooks.append(buttonAllBooks);
 
-        divAllBooks.append(h4AllBooks, imgAllBooks, pAllBooksWeeks, pAllBooksDes, aAllBooks);
-        fragment.append(divAllBooks);
+        articleAllBooks.append(h4AllBooks, imgAllBooks, pAllBooksWeeks, pAllBooksDes, aAllBooks);
+        fragment.append(articleAllBooks);
     });
     backButtonAllBooksContainer.append(backButtonAllBooks);
     sectionAllBooksContainer.append(fragment);
+
+    //Pagination Elements
+    const buttonPaginationBack = document.createElement('button');
+    buttonPaginationBack.id = 'pagination-back';
+    buttonPaginationBack.innerText = '<';
+    const spanPaginationPages = document.createElement('span');
+    spanPaginationPages.innerText = `${currentPage} of ${amountOfPages}`;
+    const buttonPaginationForward = document.createElement('button');
+    buttonPaginationForward.id = 'pagination-forward';
+    buttonPaginationForward.innerText = '>';
+
+    if (currentPage === 1) {
+        buttonPaginationBack.setAttribute('disabled', true);
+    } else {
+        buttonPaginationBack.removeAttribute('disabled');
+    }
+
+    if (currentPage === amountOfPages) {
+        buttonPaginationForward.setAttribute('disabled', true)
+    } else {
+        buttonPaginationForward.removeAttribute('disabled');
+    }
+
+    fragment.append(buttonPaginationBack, spanPaginationPages, buttonPaginationForward);
+    paginationContainer.append(fragment);
+
     loader.classList.add('hide');
 }
 
